3d3836a8e749bfa337b8a1054f354c7904d8723c
zainab <zainab122526@gmail.com>
2025-06-06 11:15:39 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+# this setup.py is responsible in creating machine learning application as a package and even deploye in pypi
+
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> list:
+    """
+    This function will return the list of requirements
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Zainab',
+    author_email='zainab122526@gmail.com',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
